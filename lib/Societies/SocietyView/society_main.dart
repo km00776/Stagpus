aeae9ee80cb137fd41efdd0d96df0f1ff139ddc9
km00776 <kmurugathas@gmail.com>
2020-07-25 23:32:30 +0100

    Added category aspect of society

---
+++ lib/Societies/SocietyView/society_main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+class SocietyScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return Scaffold(
+      
+
+    );
+  }
+}
